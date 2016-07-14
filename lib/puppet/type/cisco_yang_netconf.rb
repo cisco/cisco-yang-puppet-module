05a3431922bf53b803ec621b732e473c5e32a961
sushirol <sushirol@cisco.com>
2016-07-14 15:18:02 +0000

    If source is nil, copy source from title.

--- lib/puppet/type/cisco_yang_netconf.rb
+++ lib/puppet/type/cisco_yang_netconf.rb
@@ -94,6 +94,8 @@ Puppet::Type.newtype(:cisco_yang_netconf) do
   #
   validate do
     fail("The 'target' parameter must be set in the manifest.") if self[:target].nil?
-    fail("The 'source' property must be set in the manifest.") if self[:source].nil?
+    if self[:source].nil?
+      self[:source] = self[:target]
+    end
   end
 end
