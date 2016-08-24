dc8bf5e3c52fe30b5a67ccd3e5c0e5ff088957da
Sushrut <sushirol@cisco.com>
2016-08-24 18:55:41 +0000

    Fixed a bug where bealer test wouldn't put escape character while issuing a sudo sh " cmd " for a non-root user

--- tests/beaker_tests/cisco_yang_netconf/test_replace_vrf.rb
+++ tests/beaker_tests/cisco_yang_netconf/test_replace_vrf.rb
@@ -29,8 +29,13 @@ skip_unless_supported(tests)
 
 step 'Setup' do
   clear_vrf
-  on(agent, puppet_resource('cisco_yang_netconf', '\'' + BLUE_VRF_WO_PROPERTY + '\'', 'mode=merge'))
-  on(agent, puppet_resource('cisco_yang_netconf', '\'' + GREEN_VRF_WO_PROPERTY + '\'', 'mode=merge'))
+  title_string = BLUE_VRF_WO_PROPERTY
+  cmd = PUPPET_BINPATH + "resource cisco_yang_netconf '#{title_string}' mode=merge"
+  on(agent, cmd)
+
+  title_string = GREEN_VRF_WO_PROPERTY
+  cmd = PUPPET_BINPATH + "resource cisco_yang_netconf '#{title_string}' mode=merge"
+  on(agent, cmd)
 end
 
 teardown do
