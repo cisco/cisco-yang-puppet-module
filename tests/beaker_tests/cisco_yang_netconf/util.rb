223e5d942fdc16055eb9e32ed6388350c877c266
Sushrut <sushirol@cisco.com>
2016-08-24 18:55:13 +0000

    Fixed a bug where bealer test wouldn't put escape character while issuing a sudo sh " cmd " for a non-root user

--- tests/beaker_tests/cisco_yang_netconf/util.rb
+++ tests/beaker_tests/cisco_yang_netconf/util.rb
@@ -214,11 +214,15 @@ SRLG_GE_01_UPDATE = \
 DELETE_SRLG = '<srlg xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-infra-rsi-cfg" xmlns:xc="urn:ietf:params:xml:ns:netconf:base:1.0" xc:operation="delete"/>'
 
 def clear_vrf
-  on(agent, puppet_resource('cisco_yang_netconf', '\'' + ROOT_VRF + '\'', 'source=' + '\'' + ROOT_VRF + '\'', 'mode=replace'))
+  title_string = ROOT_VRF
+  cmd = PUPPET_BINPATH + "resource cisco_yang_netconf '#{title_string}' source='#{title_string}' mode=replace"
+  on(agent, cmd)
 end
 
 def clear_srlg
-  on(agent, puppet_resource('cisco_yang_netconf', '\'' + ROOT_SRLG + '\'', 'source=' + '\'' + ROOT_SRLG + '\'', 'mode=replace'))
+  title_string = ROOT_SRLG
+  cmd = PUPPET_BINPATH + "resource cisco_yang_netconf '#{title_string}' source='#{title_string}' mode=replace"
+  on(agent, cmd)
 end
 
 def massage_path(str)
